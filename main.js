5340679ce1646ceed911b8a198f9be025058e71a
Gabriele Zucchi <19gabrielezucchi99@gmail.com>
2024-02-27 18:45:33 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+// let num1 = 12;
+
+// let num2 = 23;
+
+
+// console.log(`la variabile num1 é uguale a ${num1}`);
+
+// console.log(`la variabile num1 é uguale a ${num2}`);
+
+// let stringa = 'ciao sono Gabriele';
+
+// document.write(`il valore della variabile stringa é: ${stringa} <br>`);
+
+// stringa = 'Non mi chiamo Gabriele ma Gianluca';
+
+// document.write(stringa);
+
+// const pigreco = 3.14159265359;
+
+// console.log(`il valore pigreco é: ${pigreco}`);
